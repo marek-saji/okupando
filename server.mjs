ab0e2969d938e1a97a8bb977e046b10ce9916c0d
Marek ‘saji’ Augustynowicz <marek.aug@gmail.com>
2019-02-19 01:04:25 +0100

    HTTPS
    Closes https://github.com/DietLabs/okupando/issues/8

--- server.mjs
+++ server.mjs
@@ -1,3 +1,4 @@
+import greenlock from 'greenlock-express';
 import './lib/cli-env';
 import { values as args } from './lib/args-definitions';
 import app from './lib/express';
@@ -6,10 +7,45 @@ import { monitorStatus } from './lib/push';
 
 monitorStatus();
 
-app.listen(args.HTTP_PORT, args.HOST, () => {
-    console.log(
-        'HTTP',
-        'Listening on',
-        `http://${args.HOST}:${args.PORT}`
+if (args.HTTPS_PORT)
+{
+    greenlock.create({
+        server: 'https://acme-v02.api.letsencrypt.org/directory',
+        version: 'draft-11',
+        email: args.ACME_EMAIL,
+        configDir: '~/.config/acme',
+        agreeTos: true,
+        communityMember: false,
+        telemetry: true,
+        servername: args.HOST,
+        approveDomains: [args.HOST],
+        app,
+    }).listen(
+        args.HTTP_PORT,
+        args.HTTPS_PORT,
+        () => {
+            console.log(
+                'HTTP',
+                'Listening on',
+                `http://${args.HOST}:${args.HTTP_PORT}`,
+            );
+        },
+        () => {
+            console.log(
+                'HTTP',
+                'Listening on',
+                `https://${args.HOST}:${args.HTTPS_PORT}`
+            );
+        },
     );
-});
+}
+else
+{
+    app.listen(args.HTTP_PORT, args.HOST, () => {
+        console.log(
+            'HTTP',
+            'Listening on',
+            `http://${args.HOST}:${args.HTTP_PORT}`
+        );
+    });
+}
